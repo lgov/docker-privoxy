735d9b3d5eed66a45025818ed2881063a3917f81
Lieven Govaerts <lgo@mobsol.be>
2020-07-26 20:20:52 +0200

    scripts/restart_privoxy_if_unhealthy.sh: add script to restart the nordvpn containers if needed.

--- scripts/restart_privoxy_if_unhealthy.sh
+++ scripts/restart_privoxy_if_unhealthy.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
 NORDVPN_SERVICE="nordvpn_nordvpn_1"
-SVC_HEALTHY=`docker ps | grep NORDVPN_SERVICE | grep "(healthy)" | wc -l`
+SVC_HEALTHY=`docker ps | grep $NORDVPN_SERVICE | grep "(healthy)" | wc -l`
 
 if [[ SVC_HEALTHY -eq 0 ]]
 then
     echo "Nordvpn unhealthy, retarting the service and the proxy"
-    docker service update --force NORDVPN_SERVICE
-    docker stop nordvpn_nordvpn-privoxy_1
-    docker restart nordvpn_nordvpn_1
+        docker stop nordvpn_nordvpn-privoxy_1
+    docker restart $NORDVPN_SERVICE
     docker start nordvpn_nordvpn-privoxy_1
 fi
 
